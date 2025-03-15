27718f768e9a7f54e51684cbf5552b899bc7bb14
unknown <gldgayathri@gmail.com>
2025-03-15 14:15:45 +0530

    added custom properties

--- 05-custom-properties-demo/src/main/java/com/gayathri/myfirstspringboot/myfirstapp/rest/FirstRestController.java
+++ 05-custom-properties-demo/src/main/java/com/gayathri/myfirstspringboot/myfirstapp/rest/FirstRestController.java
@@ -1,10 +1,24 @@
 package com.gayathri.myfirstspringboot.myfirstapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstRestController {
+
+    //inject custom properties
+    @Value("${animal.name}")
+    private String animal;
+    @Value("${animal.ecosystem}")
+    private String ecosystem;
+
+    //expose new endpoint that returns animal infp "animalinfo"
+    @GetMapping("/animalinfo")
+    public String myFavAnimal(){
+        return "Animal: " + animal + ", " + "Ecosystem: "+ ecosystem;
+    }
+
     //expose "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello(){
